4653a65243345a1db75e9a8520a02a86942191af
Matthew Hansen <mattie02@gmail.com>
2018-05-27 13:01:22 -0700

    Core completion
    Complete all core pieces

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,16 +1,18 @@
 <?php 
-    Class Pages {
+    Class Pages extends Controller {
         public function __construct(){
-            
+
         }
 
         public function index(){
 
+            $data = [
+                'title' => 'welcome'
+            ];
+
+            $this->view('pages/index', $data);
         }
 
-        public function about($id){
-            echo $id; 
-        }
 
     }
 
